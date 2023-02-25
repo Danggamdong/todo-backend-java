7fcbc5a9882225365fba2958f25d28baa775b393
tunkcalb <wjdgusdndi@gmail.com>
2023-02-25 16:03:41 +0900

    return todolist

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,27 +5,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
-	public static HashMap<String,List<String>> board = new HashMap<>();
+	public static HashMap<String,List<HashMap<String,Object>>> board = new HashMap<>();
 	public static String username = "TestUser";
-	public static String todo = "hanging leg raise";
-	public static List<String> todolist = new ArrayList<>();
+	public static List<HashMap<String,Object>> todolist = new ArrayList<>();
+	public static HashMap<String,Object> todo;
 
 	public static void main(String[] args) {
+		Properties defaultProperties = new Properties();
+		defaultProperties.put("server.port","12345" );
+		defaultProperties.put("server.address", "0.0.0.0");
+		todo = new HashMap<>();
+		todo.put("id", 1);
+		todo.put("description", "squat");
 		todolist.add(todo);
-		todolist.add("squat");
+		todo = new HashMap<>();
+		todo.put("id", 2);
+		todo.put("description", "hanging leg raise");
+		todolist.add(todo);
+
 		board.put(username, todolist);
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication sa = new SpringApplication(DemoApplication.class);
+		sa.setDefaultProperties(defaultProperties);
+		sa.run();
 	}
 	@GetMapping("/Todolist")
-	public List<String> Todolist(@RequestParam("name") String name) {
+	public List<HashMap<String,Object>> Todolist(@RequestParam("name") String name) {
 		return board.get(name);
 	}
 }
