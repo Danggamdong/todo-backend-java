19ccbaaf0a3d25d2de531f0da73a0914cb821ddc
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2023-04-23 23:36:40 +0900

    chore: properties (#7)
    * chore: properties
    
    * chore: new line

--- src/main/java/com/example/demo/TodoApplication.java
+++ src/main/java/com/example/demo/TodoApplication.java
@@ -3,17 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Properties;
-
 @SpringBootApplication
 public class TodoApplication {
+
     public static void main(String... args) {
-        Properties defaultProperties = new Properties();
-        defaultProperties.put("server.port", "80");
-        defaultProperties.put("server.address", "0.0.0.0");
 
         SpringApplication sa = new SpringApplication(TodoApplication.class);
-        sa.setDefaultProperties(defaultProperties);
         sa.run();
     }
 }
