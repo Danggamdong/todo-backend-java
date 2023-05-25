6eaa0479cbdc161316229737f0618723ac89be65
tunkcalb <95354899+tunkcalb@users.noreply.github.com>
2023-05-25 15:46:38 +0900

    Chore:refactoring (#9)
    * chore:refactoring
    
    * chore:style
    
    * chore:gitignore
    
    * fix:rename

--- src/test/java/com/example/demo/TodoDaoImplTest.java
+++ src/test/java/com/example/demo/TodoDaoImplTest.java
@@ -1,9 +1,8 @@
 package com.example.demo;
 
-import com.example.demo.dao.TodoDao;
+import com.example.demo.dao.TodoDaoImpl;
 import com.example.demo.dto.Todo;
 import org.assertj.core.api.Assertions;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,9 +13,9 @@ import java.util.HashMap;
 import java.util.List;
 
 @SpringBootTest
-public class TodoDaoTest {
+public class TodoDaoImplTest {
     @Autowired
-    TodoDao dao;
+    TodoDaoImpl dao;
 
     @Before
     public void cleanup() {
@@ -67,7 +66,6 @@ public class TodoDaoTest {
         hashMap.put("title", newTitle);
         String id = dao.update(hashMap);
         List<Todo> todoList = dao.findAll();
-        System.out.println(dao.findAll().size());
         Todo updatedTodo = todoList.get(0);
 
         Assertions.assertThat(id).isEqualTo(updateTodo.getId());
